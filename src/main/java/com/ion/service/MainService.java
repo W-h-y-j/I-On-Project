9229552459d6de08dd563699e4d3ea12840039e3
JY <wodufdla@naver.com>
2022-03-11 11:49:27 +0900

    센터찾기

--- src/main/java/com/ion/service/MainService.java
+++ src/main/java/com/ion/service/MainService.java
@@ -2,6 +2,7 @@ package com.ion.service;
 
 import java.util.List;
 
+import com.ion.vo.CenterVO;
 import com.ion.vo.MainVO;
 
 public interface MainService {
@@ -10,6 +11,10 @@ public interface MainService {
 	
 	// MainVO 타입만 담을 수 있는 list 타입으로 메서드 정의 하고 Mapper 파일에서 불러올 sql문의 id를 매서드 명으로 입력해준다. 
 	public List<MainVO> view_system_notice();
+
+	public List<CenterVO> getCenterList(CenterVO b);
+
+	public int getListCount(CenterVO b);
 	
 	
 }
--- src/main/java/com/ion/vo/MainVO.java
+++ src/main/java/com/ion/vo/MainVO.java
@@ -12,5 +12,18 @@ public class MainVO {
 	// sample로 넣은 시스템 공지사항
 	private String title;
 	private Date insert_dt;
+	
+	//center_location
+	private String center_name;
+	private String center_add;
+	private String center_tel;
+	
+	//페이징 쪽나누기 관련변수
+    private int startrow;//시작행 번호
+    private int endrow;//끝행 번호
+    
+    //검색기능
+    private String find_name;//검색어
+    private String find_field;//검색필드
 
 }
